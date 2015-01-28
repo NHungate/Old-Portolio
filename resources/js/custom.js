6f0012667bfb635fd7e7ddffa369dd7271d771ec
Nathan Hungate <Nathan.J.Hungate@gmail.com>
2015-01-28 14:43:00 -0700

    Make intro background compatible with iOS. Change background.

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,11 +1,24 @@
 var setSectionHeight = function () {
-    if ($(window).height() >= 500) {
+    if ($(window).height() >= 675) {
         var windowHeight = $(window).height() - $(".header").height();
         $("section").height(windowHeight);
     } else {
-        $("section").height(500);
+        $("section").height(675);
     }
 };
 
 setSectionHeight();
 $(window).resize(setSectionHeight);
+
+$("nav a, .logo").click(function (event) {
+    event.preventDefault();
+    if (event.currentTarget.className === "logo") {
+        $('html, body').animate({
+            scrollTop: $("#intro").offset().top
+        }, 400);
+    } else {
+        $('html, body').animate({
+            scrollTop: $(event.currentTarget.hash).offset().top
+        }, 400);
+    }
+});
