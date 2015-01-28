80b2eb351e5e51eff1f86c15055eaefe4e339427
Nathan Hungate <Nathan.J.Hungate@gmail.com>
2015-01-28 15:12:45 -0700

    Add mini-nav icon. Add jQuery to handle mini-nav clicks.

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -12,10 +12,13 @@ $(window).resize(setSectionHeight);
 
 $("nav a, .logo").click(function (event) {
     event.preventDefault();
+    console.log(event);
     if (event.currentTarget.className === "logo") {
         $('html, body').animate({
             scrollTop: $("#intro").offset().top
         }, 400);
+    } else if (event.currentTarget.id === "mini-nav") {
+        console.log("Clicked Mini Nav");
     } else {
         $('html, body').animate({
             scrollTop: $(event.currentTarget.hash).offset().top
